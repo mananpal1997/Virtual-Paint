20d45d73d77f3d7d227d499c03c622475ccd114d
Manan Pal Singh <brunodicosta@gmail.com>
2016-11-14 15:20:48 +0530

    minor changes

--- C++ Version/showVideo.cpp
+++ C++ Version/showVideo.cpp
@@ -5,22 +5,22 @@
 
 ShowVideo::ShowVideo(QObject *parent): QObject(parent)
 {
-	lower_bound = cvScalar(20, 100, 100);
-	upper_bound = cvScalar(40, 225, 225);
-	
-	line_thickness = 2;
-	area_limit = 700.00;
+    lower_bound = cvScalar(20, 100, 100);
+    upper_bound = cvScalar(40, 225, 225);
+    
+    line_thickness = 2;
+    area_limit = 700.00;
 
-	image_panel = cvLoadImage("../resources/paint.png", CV_LOAD_IMAGE_COLOR);
+    image_panel = cvLoadImage("../resources/paint.png", CV_LOAD_IMAGE_COLOR);
 
-	drawing = cvCreateImage(cvSize(640, 480), 8L, 3);
+    drawing = cvCreateImage(cvSize(640, 480), 8L, 3);
 
-	scribble = cvCreateImage(cvSize(640, 480), 8L, 3);
+    scribble = cvCreateImage(cvSize(640, 480), 8L, 3);
 
-	cvInitFont(&font, CV_FONT_HERSHEY_COMPLEX, 1, .6, 0, 2, CV_AA);
-	cvInitFont(&big_font, CV_FONT_HERSHEY_COMPLEX, 1, 1, 0, 3, CV_AA);
+    cvInitFont(&font, CV_FONT_HERSHEY_COMPLEX, 1, .6, 0, 2, CV_AA);
+    cvInitFont(&big_font, CV_FONT_HERSHEY_COMPLEX, 1, 1, 0, 3, CV_AA);
 
-	x_pos=0, y_pos=0;
+    x_pos=0, y_pos=0;
 
     edit_mode = false;
 }
@@ -46,14 +46,14 @@ void ShowVideo::startVideo()
         edit_mode = false;
     }
 
-	while(run_video)
-	{
-		frame = cvQueryFrame(capture);
-		
-		if(!frame)
-			continue;
+    while(run_video)
+    {
+        frame = cvQueryFrame(capture);
+        
+        if(!frame)
+            continue;
 
-		cvFlip(frame, NULL, 1);
+        cvFlip(frame, NULL, 1);
         cvSmooth(frame, frame, CV_MEDIAN, 5, 5);
 
         IplImage *threshold_image = get_threshold_image(frame, lower_bound, upper_bound);
@@ -71,7 +71,7 @@ void ShowVideo::startVideo()
         int last_x = x_pos, last_y = y_pos;
         x_pos = 0, y_pos = 0;
 
-        if(moment10/area >= 0 && moment10/area < 1280 && moment01/area >= 0 && moment01/area < 1280 && area > area_limit)
+        if(moment10/area >= 0 && moment10/area < 640 && moment01/area >= 0 && moment01/area < 480 && area > area_limit)
         {
             x_pos = moment10/area;
             y_pos = moment01/area;
@@ -127,7 +127,11 @@ void ShowVideo::startVideo()
         }
 
         else if(x_pos < 90 and y_pos > 130 and y_pos < 390)
+        {
             line_thickness = 6 - (y_pos/60 - 1);
+            if(line_thickness == 1)
+                line_thickness = 2;
+        }
 
         sprintf(msg, "%d", line_thickness);
         cvPutText(frame, msg, cvPoint(40, 255), &big_font, line_color);
@@ -140,28 +144,36 @@ void ShowVideo::startVideo()
         if(magnitude > 0 and magnitude < 100 and x_pos > 120 and x_pos < 530)
         {
             if(compare(line_color, white))
-                cvLine(drawing, cvPoint(x_pos, y_pos), cvPoint(last_x, last_y), line_color, line_thickness+6, CV_AA);
+                cvLine(drawing, cvPoint(x_pos, y_pos), cvPoint(last_x, last_y), line_color, line_thickness+2, CV_AA);
             else
                 cvLine(drawing, cvPoint(x_pos, y_pos), cvPoint(last_x, last_y), line_color, line_thickness, CV_AA);
         }
 
-        cvAdd(drawing, scribble, drawing);
+        // cvAdd(drawing, scribble, drawing);
         cvAnd(frame, drawing, frame);
         cvAnd(image_panel, frame, frame);
+
+        if(compare(line_color, white))
+        {
+            cvRectangle(frame, cvPoint(x_pos-line_thickness*2, y_pos), cvPoint(x_pos+line_thickness*2, y_pos+4), black);
+            cvRectangle(frame, cvPoint(x_pos-line_thickness*2, y_pos+4), cvPoint(x_pos+line_thickness*2, y_pos+4+line_thickness*8), black);
+        }
+        else
+            cvCircle(frame, cvPoint(x_pos, y_pos), line_thickness+8, black);
 
         if(drawing)
             emit sendImage(IplImagetoQImage(drawing));
 
-		emit sendFeed(IplImagetoQImage(frame));
-		int c = cvWaitKey(10);
-	}
+        emit sendFeed(IplImagetoQImage(frame));
+        int c = cvWaitKey(10);
+    }
 }
 
 void ShowVideo::stopVideo()
 {
-	cvReleaseCapture(&(capture));
-	emit sendFeed(QImage());
-	run_video = false;
+    cvReleaseCapture(&(capture));
+    emit sendFeed(QImage());
+    run_video = false;
 }
 
 void ShowVideo::saveImage()
